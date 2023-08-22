cfb921e1c72f7014b895848cdbe5865571bc8126
Kanha <129910146+kanhathakur8665@users.noreply.github.com>
2023-08-22 08:44:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SecondSmallestElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 5, 8, 3, 7, 2 };
+
+            
+
+            int smallest = int.MaxValue;
+            int secondSmallest = int.MaxValue;
+
+            foreach (int num in numbers)
+            {
+                if (num < smallest)
+                {
+                    secondSmallest = smallest;
+                    smallest = num;
+                }
+                else if (num < secondSmallest && num != smallest)
+                {
+                    secondSmallest = num;
+                }
+            }
+
+            Console.WriteLine($"The second smallest element is: {secondSmallest}");
+            Console.ReadLine();
+        }
+    }
+}
+
